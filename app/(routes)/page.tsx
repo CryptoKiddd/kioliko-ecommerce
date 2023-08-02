78062dfac3688fa1000563e8246059adaf44fc23
CryptoKiddd <avaliani1232@gmail.com>
2023-08-03 02:27:43 +0400

    billboards

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,8 +1,17 @@
-const HomePage = ()=>{
+import getBillboards from "@/actions/get-billboards";
+import Billboard from "@/components/billboard"
+import Container from "@/components/ui/Container"
+
+export const revalidate = 0;
+
+const HomePage = async()=>{
+    const billboard = await getBillboards('53360b8c-6816-485a-95f4-59a77023344e')
     return(
-        <div>
-            mainpage
-        </div>
+        <Container>
+            <div className="space-y-10 pb-10">
+                <Billboard data={billboard} />
+            </div>
+        </Container>
     )
 }
 export default HomePage
