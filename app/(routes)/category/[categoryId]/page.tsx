2a3fb8196ab0f84ce4c10187ba6fdaa7fcf56ef0
CryptoKiddd <avaliani1232@gmail.com>
2023-08-06 00:19:44 +0400

    category mobile filters

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -4,7 +4,10 @@ import getProducts from "@/actions/get-products"
 import getSizes from "@/actions/get-sizes"
 import Billboard from "@/components/billboard"
 import Container from "@/components/ui/Container"
+import NoResults from "@/components/ui/no-results"
+import ProductCard from "@/components/ui/Product-card"
 import Filter from "./_components/filter"
+import MobileFilters from "./_components/mobile-filters"
 
 export const revalidate = 0
 
@@ -36,6 +39,7 @@ const CategoryPage = async ({
             <Billboard data={category.billboard} />
             <div className="px-4 sm:px-6 lg:px-8 pb-24">
                 <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
+                    <MobileFilters  colors={colors} sizes={sizes} />
                  <div className="hidden lg:block">
                     <Filter
                     valueKey="sizeId"
@@ -48,6 +52,14 @@ const CategoryPage = async ({
                     data={colors}
                     />
                  </div>
+                 <div className="mt-6 lg:col-span-4 lg:mt-0">
+              {products.length === 0 && <NoResults />}
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                {products.map((item) => (
+                  <ProductCard key={item.id} data={item} />
+                ))}
+              </div>
+            </div>
 
                 </div>
             </div>
